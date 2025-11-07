68a4b69fcacfe19c1b3a37b57b30970f3d77e540
Berkay Öztürk <127662417+Berkayozturkx@users.noreply.github.com>
2025-11-07 13:56:46 +0300

    Remove Cloudinary API credentials
    Clear sensitive Cloudinary credentials.

--- lib/config/cloudinary_config.dart
+++ lib/config/cloudinary_config.dart
@@ -1,6 +1,6 @@
 class CloudinaryConfig {
-  static const String cloudName = 'dajrpxkhq';
-  static const String apiKey = '386211652126275';
-  static const String apiSecret = 'n6rsoSOGSJAcYiay0xdxK6WPUEI';
+  static const String cloudName = '';
+  static const String apiKey = '';
+  static const String apiSecret = '';
   static const String uploadPreset = 'izpano';
 }
